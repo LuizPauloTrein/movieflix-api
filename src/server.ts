e1a4dab5247c37ffa1898c7a918f0f984e29926c
Luiz <luiz.p.trein@gmail.com>
2026-08-27 23:45:14 -0300

    configuracoes express

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,15 @@
-import { PrismaClient } from "../generated/prisma/client"
+import express from "express"
 
-const primsa = new PrismaClient()
+const port = 3000
+const app = express()
+
+// get, post, put, patch, delete
+
+app.get("/movies", (req, res) => {
+  res.send("listagem de filmes")
+})
+
+app.listen(port, () => {
+  console.log(`servidor em execucao na ${port}`);
+  
+})
