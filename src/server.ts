d30c23d8bee3aa6de7f6d4d902e57582f02b5b26
Luiz <luiz.p.trein@gmail.com>
2026-09-02 23:30:14 -0300

    feat: add mensagem de erro na criacao de filme

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,8 @@ const port = 3000
 const app = express()
 const prisma = new PrismaClient()
 
+app.use(express.json())
+
 // get, post, put, patch, delete
 
 app.get("/movies", async (_, res) => {
@@ -18,6 +20,27 @@ app.get("/movies", async (_, res) => {
     },
   })
   res.json(movies)
+})
+
+app.post("/movies", async (req, res) => {
+  const { title, genre_id, language_id, oscar_count, release_date } = req.body
+
+  try {
+    await prisma.movie.create({
+      data: {
+        title,
+        genre_id,
+        language_id,
+        oscar_count,
+        release_date: new Date(release_date),
+      },
+    })
+  } catch (error) {
+    console.log(error)
+    return res.status(500).send({ message: "falha ao cadastrar um filme" })
+  }
+
+  res.status(201).send()
 })
 
 app.listen(port, () => {
