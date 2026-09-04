e15e40db86fbd8875a1ef89539392099fbc74e08
Luiz <luiz.p.trein@gmail.com>
2026-09-03 21:34:57 -0300

    feat: add atualizacao de filmes

--- src/server.ts
+++ src/server.ts
@@ -53,6 +53,39 @@ app.post("/movies", async (req, res) => {
   res.status(201).send()
 })
 
+app.put("/movies/:id", async (req, res) => {
+  const id = Number(req.params.id)
+
+  try {
+    const movie = await prisma.movie.findUnique({
+      where: {
+        id,
+      },
+    })
+
+    if (!movie) {
+      return res.status(404).send({ message: "filme nao encontrado" })
+    }
+
+    const data = { ...req.body }
+    data.release_date = data.release_date
+      ? new Date(data.release_date)
+      : undefined
+    await prisma.movie.update({
+      where: {
+        id,
+      },
+      data: data,
+    })
+  } catch (error) {
+    console.log(error)
+    return res
+      .status(500)
+      .send({ message: "falha ao atualizar o registro do filme" })
+  }
+  res.status(200).send()
+})
+
 app.listen(port, () => {
   console.log(`servidor em execucao na ${port}`)
 })
